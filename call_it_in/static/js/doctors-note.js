5d84067b0bd78ca11135b1a18e66e78ca63e1c22
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-06-30 01:49:41 -0600

    ...

---
+++ call_it_in/static/js/doctors-note.js
@@ -0,0 +1,13 @@
+
+document.querySelector(".doctors-note-form").addEventListener("submit", function() {
+
+    if (document.querySelector("#fname").value == "" 
+        | document.querySelector("#lname").value == ""
+        | document.querySelector("#lname").value == "") {
+            if (alert("Please enter your first and last name:")) {
+                
+            } 
+    };
+
+})
+
