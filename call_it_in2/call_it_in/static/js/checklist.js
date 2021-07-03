23898935a8e7cb6548d7df2dabc087c2bd61c8ca
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-07-03 13:18:37 -0600

    ...

--- call_it_in2/call_it_in/static/js/checklist.js
+++ call_it_in2/call_it_in/static/js/checklist.js
@@ -1,6 +1,9 @@
 var myNodelist = document.getElementsByTagName("LI");
+// myNodelist should come from localStorage
+
 var i;
 
+// add the x's to the li's
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -9,18 +12,23 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
+// make the close funtion work
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
+    // delete from localStorage
   }
 }
 
+// adding  new item
 document.querySelector(".checklist-form-submit").addEventListener("click", function() {
 var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
+    localStorage.setItem('excuse', inputValue);
+
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
@@ -42,14 +50,15 @@ var li = document.createElement("li");
       close[i].onclick = function() {
         var div = this.parentElement;
         div.style.display = "none";
+        // delete from localStorage
       }
     }
 });
-
 
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
+    // add a checked attribute to the localStorage 
   }
 }, false);
